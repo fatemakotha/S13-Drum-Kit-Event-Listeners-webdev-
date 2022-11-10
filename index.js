ad41e3a027d5cf5edd5360188089f8c7ef83713f
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 18:50:59 +0600

    Play same audio for all buttons

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length; //gives 7
 //Create a for loop for these 7 buttons:
 for (var i = 0; i<numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    alert("I got clicked");
+
+    var audio = new Audio("sounds/tom-1.mp3");
+    audio.play();
+
   }); //addEventListener takes in a type of click and a function to exectue which is this case is alert
   // function() is an anonymouse function
 }
