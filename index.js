7d1afacbcbee05d5bf58edbee327448e2f79c94f
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 16:11:06 +0600

    Add event listener to all buttons with class .drum

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-document.querySelector("button").addEventListener("click", function () {
-  alert("I got clicked");
-}; //addEventListener takes in a type of click and a function to exectue which is this case is alert
-// function() is an anonymouse function
+
+//Select all with a class of drum: Target all elements that have a selector of .drum
+var numberOfDrumButtons = document.querySelectorAll(".drum").length; //gives 7
+
+//Create a for loop for these 7 buttons:
+for (var i = 0; i<numberOfDrumButtons; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    alert("I got clicked");
+  }); //addEventListener takes in a type of click and a function to exectue which is this case is alert
+  // function() is an anonymouse function
+}
