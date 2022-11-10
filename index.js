9f1107a59fcdbf8f71088dacb2dded8c175c2070
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 15:57:07 +0600

    Comment

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 document.querySelector("button").addEventListener("click", function () {
   alert("I got clicked");
 }; //addEventListener takes in a type of click and a function to exectue which is this case is alert
+// function() is an anonymouse function
