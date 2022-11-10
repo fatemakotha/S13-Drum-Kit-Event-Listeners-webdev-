0d601a07234e3f7f9741fa5f2d90d8840507592a
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 15:56:32 +0600

    Add function to event listener

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-document.querySelector("button").addEventListener("click", handleClick); //click is the event, and handleClick is the function
-
-function handleClick() {
+document.querySelector("button").addEventListener("click", function () {
   alert("I got clicked");
-}
+}; //addEventListener takes in a type of click and a function to exectue which is this case is alert
