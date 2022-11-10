b53bab92760b90baab0c0d3c3f5e4f33be02add2
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 19:27:04 +0600

    The use of keyword: this

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length; //gives 7
 //Create a for loop for these 7 buttons:
 for (var i = 0; i<numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    console.log(this); //the word "this" returns the button object that was presses **
 
-    var audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
 
   }); //addEventListener takes in a type of click and a function to exectue which is this case is alert
   // function() is an anonymouse function
 }
+
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
