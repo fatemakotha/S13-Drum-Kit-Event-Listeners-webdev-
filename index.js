4b17ac6ce2a13bd9b82577774fcee47bd2918b34
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-10 19:33:15 +0600

    Button text changes color when clocked

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length; //gives 7
 //Create a for loop for these 7 buttons:
 for (var i = 0; i<numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    console.log(this); //the word "this" returns the button object that was presses **
+    this.style.color = "red"; //the word "this" returns the button object that was presses ** and prints innerHTML of the button
 
 
   }); //addEventListener takes in a type of click and a function to exectue which is this case is alert
