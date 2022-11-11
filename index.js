9940510cb1cdb9d819719b2418f847bbc4b11715
fatemakotha <fatema.alam.kotha@gmail.com>
2022-11-11 19:16:43 +0600

    Refactor

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 //Select all with a class of drum: Target all elements that have a selector of .drum
 var numberOfDrumButtons = document.querySelectorAll(".drum").length; //gives 7
 
+
+//addEventListener takes in a type of click and a function to exectue which is this case is alert
+// function() is an anonymouse function
 //Create a for loop for these 7 buttons:
 for (var i = 0; i<numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-    this.style.color = "red"; //the word "this" returns the button object that was pressed ** 
+    this.style.color = "red"; //the word "this" returns the button object that was pressed **
 
 
-  }); //addEventListener takes in a type of click and a function to exectue which is this case is alert
-  // function() is an anonymouse function
+  });
 }
 
 
